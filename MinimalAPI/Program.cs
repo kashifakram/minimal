b1a995377c2664b8b269c166334bcea6b42c7847
Kashif Akram <kashif.akram@accenture.com>
2023-05-28 12:36:59 +1000

    initial commit

---
+++ MinimalAPI/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalAPI.DbContexts;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<DishContext>(o => o.UseSqlite(
+    builder.Configuration["ConnectionStrings:DishesDbCS"]));
+
+// build app from builder after adding dependency injections services
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+// redirection middleware
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+// route end point
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+});
+
+// recreate and migrate database on each for demo
+using (var serviceScope = app.Services.GetService<IServiceScopeFactory>().CreateScope())
+{
+    var context = serviceScope.ServiceProvider.GetRequiredService<DishContext>();
+    context.Database.EnsureDeleted();
+    context.Database.Migrate();
+}
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
